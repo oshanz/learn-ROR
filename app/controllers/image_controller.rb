62970d4d9afe3b6a84e5f3f3a8fcc7ace2210558
Oshanz <spiritualmirror@gmail.com>
2015-11-20 00:36:36 +0530

    img shrink done

--- app/controllers/image_controller.rb
+++ app/controllers/image_controller.rb
@@ -1,7 +1,22 @@
+require 'rubygems'
+require 'vips'
+include VIPS
+
 class ImageController < ApplicationController
 
   def create
-    
+    uploader = ImageUploader.new
+    uploader.store!(params[:image])
+    Image.new(uploader.current_path)
+      .shrink(2)
+      .write("public/#{uploader.store_dir}/2/#{uploader.filename}")
+    Image.new(uploader.current_path)
+      .shrink(4)
+      .write("public/#{uploader.store_dir}/4/#{uploader.filename}")
+    Image.new(uploader.current_path)
+      .shrink(6)
+      .write("public/#{uploader.store_dir}/6/#{uploader.filename}")
+    session[:image] = uploader.filename
     redirect_to :back
   end
 end
